5202ff8dffc5790319d1405d07d5c14e93351f26
user <user@this.com>
2020-08-18 20:43:24 -0400

    Added Token, TokenAPI, and TokenRequestData classes.

--- event_app.auth/src/main/java/com/event_app/auth/api/Token.java
+++ event_app.auth/src/main/java/com/event_app/auth/api/Token.java
@@ -1,5 +1,19 @@
 package com.event_app.auth.api;
 
 public class Token {
+	String token;
 
+	public Token(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
 }
